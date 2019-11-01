29f67ba92b73e102a41e2a3d49ec179e177e6a81
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-01 12:29:00 -0700

    update branch logic.

--- Desktop/beepboopproject/js/scripts.js
+++ Desktop/beepboopproject/js/scripts.js
@@ -1,27 +1,30 @@
 
+
 arrNumbers = [];
+function beepBoop(userInput) {
 
-for (i = 0; i < 101; i++) {
-    arrNumbers.push(i);
-   if(arrNumbers.includes(1) === true);{
-    arrNumbers.push("Beep!");
-  } else if(arrNumbers.includes(2) === true);{
-    arrNumbers.push("Boop!");
-  } else if(arrNumbers.includes(2) === true);{
-    arrNumbers.push("I'm sorry, Dave. I'm afraid I can't do that.");
-  }  
+
+for (i = 0; i <= userInput; i++) {
+  arrNumbers.push(i);
+  console.log(arrNumbers);
+ if(arrNumbers.includes(2) === true){
+  arrNumbers.push("Beep!");
+} else if(arrNumbers.includes(2) === true){
+  arrNumbers.push("Boop!");
+} else if (arrNumbers.includes(3) === true)
+  arrNumbers.push("I'm sorry, Dave. I'm afraid I can't do that.");
 }
-console.log(arrNumbers);
+};
 
 
 
 
 
-//
-// // User Interface logic
-// $(document).ready(function() {
-//   $("#userInput").submit(function(event) {
-//     event.preventDefault();
-//     var userInput = parseInt($("#userInput").val());
-//   });
-// });
+// User Interface logic
+$(document).ready(function() {
+  $("#userForm").submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("#userInput").val());
+    var results = beepBoop(userInput);
+  });
+});
