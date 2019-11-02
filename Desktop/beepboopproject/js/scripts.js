39089dd00f83e8e364b7f29f5336cb8744a321e9
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-01 19:03:45 -0700

    further change current logic for loop. Create two arrays. Going from parseInt to string.

--- Desktop/beepboopproject/js/scripts.js
+++ Desktop/beepboopproject/js/scripts.js
@@ -1,31 +1,23 @@
 
-inputNumbers = sounds.split("");
-sounds = [];
-function beepBoop(userInput) {
+function beepboop(userInput){
 
+var inputArray = [];
 for (i = 0; i < userInput; i++) {
-  sounds.push(i);
-  console.log(sounds);
-}
+    inputArray.push(i);
+  var inputString = inputString.push(inputArray);
 
-// inputNumbers.forEach(function(){
- if(userInput === 1) {
-  sounds.push("Beep!");
-  console.log(sounds);
+ if(inputString[i].includes('3')){
+  inputString[i].push("I'm sorry, Dave. I'm afraid I can't do that.");
+  console.log(inputString);
 } else if(userInput === 2){
-  sounds.push(i,"Boop!");
-  console.log(sounds);
+  inputString[i].push(i,"Boop!");
+  console.log(inputString);
 } else if (userInput === 3) {
-  sounds.push("I'm sorry, Dave. I'm afraid I can't do that.");
-  console.log(sounds);
-} else {
-  return inputNumbers
-  console.log(inputNumbers);
-}
-}
-
-
-
+  inputString[i].push("I'm sorry, Dave. I'm afraid I can't do that.");
+  console.log(inputString);
+};
+};
+};
 
 
 // User Interface logic
@@ -33,6 +25,35 @@ $(document).ready(function() {
   $("#userForm").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#userInput").val());
-    var results = beepBoop(userInput);
+    var results = beepboop(userInput);
   });
 });
+
+
+//
+// inputNumbers = sounds.split("");
+// sounds = [];
+// function beepBoop(userInput) {
+//
+// for (i = 0; i < userInput; i++) {
+//   sounds.push(i);
+//   console.log(sounds);
+// }
+//
+// // inputNumbers.forEach(function(){
+//  if(userInput === 1) {
+//   sounds.push("Beep!");
+//   console.log(sounds);
+// } else if(userInput === 2){
+//   sounds.push(i,"Boop!");
+//   console.log(sounds);
+// } else if (userInput === 3) {
+//   sounds.push("I'm sorry, Dave. I'm afraid I can't do that.");
+//   console.log(sounds);
+// } else {
+//   return inputNumbers
+//   console.log(inputNumbers);
+// }
+// }
+//
+// k= tostring(x)
